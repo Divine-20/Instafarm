ba1b6c0e9bf21e2d00a64969e86e9b7b5efc231f
Divine-20 <divineingabire69@gmail.com>
2022-07-13 20:32:53 +0200

    added icons and styled them

--- src/components/headerContent.jsx
+++ src/components/headerContent.jsx
@@ -1,30 +1,41 @@
 import React from 'react'
 import logo from '../images/logo.png'
 import image from '../images/image.jpg'
-import { FaRegHeart,FaShoppingCart } from 'react-icons/fa'
+import { FaRegHeart,FaShoppingCart,FaCaretDown,FaSearch } from 'react-icons/fa'
 function HeaderContent() {
   return (
     <div>
-        <nav className='flex h-20'>
-            <div className='flex'>
-          <img src={logo} alt = "logo" />
+        <nav className='flex h-20 '>
+            <div className='flex ml-10 mt-4'>
+          <img src={logo} alt = "logo"  className='h-20'/>
 <div className='block'>
- <h1>Instafarm</h1>
-<p>farms.</p>
+ <h1 className='text-2xl text-[#3BB77E] font-bold pt-4'>Instafarm</h1>
+<p className='text-gray-600'>farms.</p>
 </div>
 
 </div>  
-    <form action="">
-        <select name="" id="">
+<div className='mt-6 bg-[#F3F3F3] ml-20 h-20 w-auto pr-5 ' >
+    <form action="" className='flex pt-4 '>
+        <select name="" id="" className='w-44 ml-4  pl-4'>
             <option value="All categories">All categories</option>
             <option value="fruits">Fruits</option>
             <option value="vegetables">Vegetables</option>
         </select>
-         <input type="search" placeholder='Search for items'/>
+        <div className='h-6 w-[1px] bg-gray-600 ml-4 mt-2'></div>
+       <input type="search" placeholder='Search for items' className='pl-6 w-[20vw] ml-4'/>
+       <div className='bg-[#3BB77E] w-16 pl-4 pt-2'>
+           <i className='text-2xl text-white '><FaSearch /></i>
+       </div>
+         
     </form>
-           <i><FaRegHeart /></i>
-           <i><FaShoppingCart /></i>
-            <img src={image} alt="profile" />
+    </div>
+           <i className='pt-6 text-3xl'><FaRegHeart /></i>
+           <i className='pt-6 text-3xl'><FaShoppingCart /></i>
+           
+            <img src={image} alt="profile" className='rounded-full w-20 h-20'/>
+            <p className='pt-8'>Apple Red</p>
+            <i className='pt-8 text-2xl'><FaCaretDown /></i>
+
         </nav>
     </div>
   )
