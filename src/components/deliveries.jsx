bb6372292250938fd13e6062be6e51cd77b47658
Divine-20 <divineingabire69@gmail.com>
2022-07-14 13:58:00 +0200

    added an svg

--- src/components/deliveries.jsx
+++ src/components/deliveries.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Rectangle1 from '../images/Rectangle1.svg'
 
 function Deliveries() {
   return (
     <div>
-      <div className="  h-[78vh] w-full" style={{backgroundImage:`url(${Rectangle1})`}}>
+      <div className="  h-[78vh] w-2/4" style={{backgroundImage:`url(${Rectangle1})`}}></div>
        <div></div> 
     </div>
   )
