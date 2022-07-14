e693e9c945bd7fcf37eab0a6bf3d63c607112bd3
Divine-20 <divineingabire69@gmail.com>
2022-07-14 20:54:57 +0200

    added some content

--- src/components/deliveries.jsx
+++ src/components/deliveries.jsx
@@ -5,7 +5,9 @@ function Deliveries() {
   return (
     <div className='flex justify-center mt-10'>
       <div className="  h-[60vh] w-[40vw] mr-24" style={{backgroundImage:`url(${Rectangle1})`}}>
-         <div className='h-[60vh] w-[40vw] bg-[#FFF5E1] opacity-70'></div>
+         <div className='h-[60vh] w-[40vw] bg-[#FFF5E1] opacity-70'>
+
+         </div>
       </div>
       <div className="  h-[60vh] w-[40vw]" style={{backgroundImage:`url(${Rectangle1})`}}>
       <div className='h-[60vh] w-[40vw] bg-[#D2EFE1] opacity-70'></div>
