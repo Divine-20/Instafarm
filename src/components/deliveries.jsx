e5edeb5c6bc4e6a7ff8d5b8ca6849dd576fb8790
Divine-20 <divineingabire69@gmail.com>
2022-07-14 14:11:40 +0200

    added content to the div

--- src/components/deliveries.jsx
+++ src/components/deliveries.jsx
@@ -3,8 +3,13 @@ import Rectangle1 from '../images/Rectangle1.svg'
 
 function Deliveries() {
   return (
-    <div>
-      <div className="  h-[78vh] w-2/4" style={{backgroundImage:`url(${Rectangle1})`}}></div>
+    <div className='flex justify-center mt-10'>
+      <div className="  h-[60vh] w-[40vw] mr-24" style={{backgroundImage:`url(${Rectangle1})`}}>
+         <div className='h-[60vh] w-[40vw] bg-[#FFF5E1] opacity-70'></div>
+      </div>
+      <div className="  h-[60vh] w-[40vw]" style={{backgroundImage:`url(${Rectangle1})`}}>
+      <div className='h-[60vh] w-[40vw] bg-[#D2EFE1] opacity-70'></div>
+      </div>
        <div></div> 
     </div>
   )
